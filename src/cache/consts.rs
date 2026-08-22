6135bf5ae5f627927c5a4dadb88c2a077aa8217b
its-saanvi <saanviwastaken544@gmail.com>
2026-08-22 21:41:10 +0530

    fix: emoji constants

--- src/cache/consts.rs
+++ src/cache/consts.rs
@@ -1,6 +1,6 @@
 pub const NETHER_EMOJI: &'static str = "<:nether:1346126332072824962>";
-pub const FORT_EMOJI: &'static str = "<:fortress:1306295901278437397>";
-pub const PORTAL_EMOJI: &'static str = "<:portal:1306295821246922813>";
-pub const SH_EMOJI: &'static str = "<:sh:1306295768109420605>";
-pub const END_EMOJI: &'static str = "<:end:1306295918638796941>";
+pub const FORT_EMOJI: &'static str = "<:fortress:1321840167648100362>";
+pub const PORTAL_EMOJI: &'static str = "<:portal:1321840231422492682>";
+pub const SH_EMOJI: &'static str = "<:sh:1321840281292640308>";
+pub const END_EMOJI: &'static str = "<:end:1321840338314199110>";
 pub const EDIT_MESSAGE_DELAY: u64 = 5;
